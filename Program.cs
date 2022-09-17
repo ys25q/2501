95f452269b1daeb049f3654bbc77a6df3f0227d6
Bajahv23@student.itstep.org <Bajahv23@student.itstep.org>
2022-09-17 12:58:38 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker[] Worker = new Worker[]
+                        {
+                new Worker ("Petrov", "Ivan", 45, 3, "ofic"),
+                new Worker ("Petrenko", "Petro", 25, 8, "ofic"),
+                new Worker ("Ivanenko", "Ivan", 55, 7, "ofic"),
+                new Worker ("Sydorenko", "Sydr", 61, 9, "ofic"),
+
+                        };
+
+            foreach (Worker item in Worker)
+            {
+                Console.WriteLine(item.Surname);
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Age);
+                Console.WriteLine(item.Zp);
+                Console.WriteLine(item.Status);
+                Console.WriteLine(item.Zp *80.5);
+                Console.WriteLine(item.IsPensioner);
+
+                Console.WriteLine("                       ");
+
+
+
+                //Worker rec = new Worker(5, 8);
+                //Console.WriteLine(rec.Area);
+                //double area = rec.Area;
+                //Console.WriteLine(area);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        /* Rectangle1[] rec = new Rectangle1[]
+            {
+                new Rectangle1 (5, 8),
+                new Rectangle1 (3, 8),
+                new Rectangle1 (7, 8),
+                new Rectangle1 (9, 8),
+                new Rectangle1 (5, 9),
+            };
+            foreach (Rectangle1 item in rec) 
+            {
+                Console.WriteLine(item.Area);*/
+
+        /* Rectangle1 rec = new Rectangle1 (5, 8);
+         Console.WriteLine(rec.Area);
+         double area = rec.Area;
+         Console.WriteLine(area);*/
+    }
+
+}
+
---
+++ Worker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Worker
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Zp { get; set; }
+        public string Status { get; set; }
+
+        public bool IsPensioner
+        {
+            get
+            {
+                if(Age > 60)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Worker()
+        {
+        }
+
+        public Worker(string Surname, string Name, int Age, int Zp, string Status)
+        {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Age = Age;
+            this.Zp = Zp;
+            this.Status = Status;
+            
+            }
+    }
+}
